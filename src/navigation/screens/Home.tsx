12ec1615d0876c8f2effab083a9a2c71e2662747
Wojtek Wincewicz <wswin@protonmail.com>
2026-06-18 15:52:11 +0200

    Remove logs

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -759,7 +759,6 @@ export function Home({ date }: { date: Date }) {
 
   const handleMeasurmentInputSave = async (value: AssessmentValue) => {
     if (clickedScheduledMeasurment) {
-      console.log("handleMeasurmentInputSave", value, typeof value);
       if (clickedScheduledMeasurment.measurmentRecordId) {
         await dbDeleteScheduledMeasurmentRecord(
           db,
